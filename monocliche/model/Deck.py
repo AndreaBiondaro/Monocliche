fd1998e0c64817bffbda0fe776bf119218644245
AndreaBiondaro <andrea.biondaro89@gmail.com>
2021-01-10 17:26:46 +0100

    moved the logic of decks from service directly into the model class

--- monocliche/model/Deck.py
+++ monocliche/model/Deck.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 from monocliche.model import Card
 
 
@@ -5,3 +7,15 @@ class Deck:
 
     def __init__(self, cards=None):
         self.cards: list[Card] = cards
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def draw_card(self) -> Card:
+        """
+        Draw the top card from the input deck. The drawn card is moved under the deck (bottom of the list).
+        """
+
+        card = self.cards.pop(0)
+        self.cards.append(card)
+        return card
---
+++ monocliche/tests/DeckTest.py
@@ -0,0 +1,24 @@
+import unittest
+
+from monocliche.model.Card import Card
+from monocliche.model.Deck import Deck
+
+
+class DeckTest(unittest.TestCase):
+
+    def test_draw_card(self):
+        card1 = Card("title1", "", None)
+        card2 = Card("title2", "", None)
+
+        deck = Deck([card1, card2])
+
+        card = deck.draw_card()
+
+        self.assertEqual("title1", card.title)
+        self.assertEqual(2, len(deck.cards))
+        self.assertEqual("title2", deck.cards[0].title)
+        self.assertEqual("title1", deck.cards[1].title)
+
+
+if __name__ == '__main__':
+    unittest.main()
